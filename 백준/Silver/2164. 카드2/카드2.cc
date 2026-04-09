75417558a773bc9e94e9353b502eef996b23e4d6
Hyein Seo <gpdls0411@naver.com>
2026-04-09 12:04:48 +0900

    [Silver IV] Title: 카드2, Time: 4 ms, Memory: 4156 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	queue<int> Q;
+	int N;
+	cin >> N;
+
+	for (int i = 1; i <= N; i++) {
+		Q.push(i);
+	}
+
+	while (Q.size() != 1) {
+		Q.pop();
+		Q.push(Q.front());
+		Q.pop();
+	}
+
+	cout << Q.front();
+
+	return 0;
+}
